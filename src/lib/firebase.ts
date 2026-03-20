091800f5f9bdf274c4a0817ed5fcf5a4faea4acb
Vijayshree25 <vijayshreeparakh7@gmail.com>
2026-03-20 23:14:02 +0530

    added .env

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAr7-FlZZ6Z3RMuAIt34jt_03oP6mGYH8k",
-  authDomain: "rozirakshak-ai.firebaseapp.com",
-  projectId: "rozirakshak-ai",
-  storageBucket: "rozirakshak-ai.firebasestorage.app",
-  messagingSenderId: "1004603597178",
-  appId: "1:1004603597178:web:b119e25e57275b302a36c3",
-  measurementId: "G-LTJ8LVLLNL",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
